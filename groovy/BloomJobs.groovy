4fc245ccb65c6c97749951d51fad62faf576b4be
Eberhard Beilharz <eb1@sil.org>
2014-10-01 11:58:47 +0200

    Rename Bloom jobs
    Change-Id: I24f65bf6eaf37b036cee216bad7bfafe52154e02

--- groovy/BloomJobs.groovy
+++ groovy/BloomJobs.groovy
@@ -12,20 +12,20 @@ def package_version = '--package-version "\${FULL_BUILD_NUMBER}" ';
 
 /*
  * Definition of jobs
+ *
+ * NOTE: if you want to rename jobs, rename them in Jenkins first, then change the name here
+ * and commit and push the changes.
  */
 
 job {
     common.defaultPackagingJob(delegate, packagename, subdir_name, package_version, revision,
         distros_tobuild);
 
-    name 'Bloom_NightlyPackaging-Linux-all-master-debug';
+    name 'Bloom_Packaging-Linux-all-3.0-release';
 
     description '''
 <p>Nightly builds of the Bloom 3.0 branch.</p>
 <p>The job is created by the DSL plugin from <i>BloomJobs.groovy</i> script.</p>
-<p>For historic reasons the job name doesn't really match what it does. It's a build of
-the 3.0 release branch. Renaming it in the groovy script isn't possible without messing up
-the package version numbers.</p>
 ''';
 
     common.hgScm(delegate, 'https://bitbucket.org/hatton/bloom-desktop', 'bloom-3.0Linux', subdir_name);
@@ -37,7 +37,7 @@ job {
     common.defaultPackagingJob(delegate, packagename, subdir_name_unstable, package_version, revision,
         distros_tobuild);
 
-    name 'Bloom_NightlyPackaging-Linux-all-3.1-release';
+    name 'Bloom_Packaging-Linux-all-master-release';
 
     description '''
 <p>Nightly builds of the Bloom default branch.</p>
