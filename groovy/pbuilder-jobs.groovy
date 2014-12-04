cb0ee08b2b233e9ba131a386dfe2cf54d1c41282
Eberhard Beilharz <eb1@sil.org>
2014-12-04 12:43:18 +0100

    Add job to clean out old binary packages and cancelled builds
    Change-Id: Icf1f8e74c9c70e69bca16d13a5648869d53c9fc5

--- groovy/pbuilder-jobs.groovy
+++ groovy/pbuilder-jobs.groovy
@@ -36,3 +36,33 @@ $HOME/ci-builder-scripts/bash/update --no-package --dists "$Distributions"
 	}
 }
 
+job {
+    name 'PBuilder_Cleanup-Linux-all-master-debug';
+
+    description '''
+<p>Maintenance job that cleans out previously built binary packages and cancelled builds left on disk
+that are at least two days old.</p>
+<p>The job is created by the DSL plugin from <i>pbuilder-jobs.groovy</i> script.</p>
+''';
+
+	label 'packager';
+
+	logRotator(365, 100, 10, 10);
+
+	triggers {
+		// Run once every day
+		// Times are UTC
+		cron("H H * * *");
+	}
+
+	wrappers {
+		timestamps();
+	}
+
+	steps {
+		shell('''
+$HOME/ci-builder-scripts/bash/clean-old-builds --no-package
+''');
+	}
+}
+
