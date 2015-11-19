40653950ebb2fa195b7272837ab2b893ed7fa24b
Eberhard Beilharz <eb1@sil.org>
2015-11-19 15:43:55 +0100

    Fix description again
    Change-Id: I255cce648155d8d505c7f59f10bf1d8a20985058

--- groovy/Infrastructure_jobs.groovy
+++ groovy/Infrastructure_jobs.groovy
@@ -2,8 +2,6 @@
  * DSL script to create Infrastructure jobs
  */
 
-def thisFile = getClass().protectionDomain.codeSource.location.path
-
 /*
  * Definition of jobs
  */
@@ -12,7 +10,7 @@ freeStyleJob('Infrastructure-Linux-master-debug-checkoutonly') {
 
 	description '''
 <p>This job keeps the <i>docker</i> directory up-to-date on the packaging machine.</p>
-<p>The job is created by the DSL plugin from <i>''' + thisFile + '''</i> script.<p>
+<p>The job is created by the DSL plugin from <i>Infrastructure_jobs.groovy</i> script.<p>
 '''
 
 	label 'master';
