f6b7fd4d93b726c2d5b27e3647907eb678d528c9
Eberhard Beilharz <eb1@sil.org>
2016-02-12 12:09:21 +0100

    Add Infrastructure view
    Change-Id: I78a94672de5bb0b62dfdb33384b5f5bc3984d978

---
+++ groovy/Infrastructure_views.groovy
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2016 SIL International
+ * This software is licensed under the MIT license (http://opensource.org/licenses/MIT)
+ */
+/*
+ * DSL script for Infrastructure Jenkins views
+ */
+
+/* Definition of views */
+
+listView('Infrastructure') {
+	description 'Infrastructure jobs'
+	filterBuildQueue false
+	filterExecutors true
+
+	jobs {
+		regex('^Infrastructure|Infrastructure/.*')
+	}
+
+	columns {
+		status()
+		weather()
+		name()
+		lastSuccess()
+		lastFailure()
+		lastDuration()
+		buildButton()
+		lastBuildNode()
+		lastBuildConsole()
+	}
+	configure { view ->
+		view / columns / 'hudson.plugins.nodenamecolumn.NodeNameColumn'
+	}
+}
