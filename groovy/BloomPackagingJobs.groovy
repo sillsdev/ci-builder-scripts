69265c533021cd516eb1573b2315ed50702aaec0
Eberhard Beilharz <eb1@sil.org>
2016-04-20 11:01:51 +0200

    [bloom] Add Xenial to distros to build
    Also remove unnecessary semicolons.
    
    Change-Id: I7491fe29495d8bef8ac441e60c519ef3ec1afdd0

--- groovy/BloomPackagingJobs.groovy
+++ groovy/BloomPackagingJobs.groovy
@@ -4,16 +4,16 @@
 import utilities.common
 
 // Variables
-def packagename = 'Bloom';
-def subdir_name = 'bloom-desktop';
-def subdir_name_beta = 'bloom-desktop-beta';
-def subdir_name_unstable = 'bloom-desktop-unstable';
-def distros_tobuild = 'precise trusty';
-def repo = 'git://github.com/BloomBooks/BloomDesktop.git';
-def email_recipients = 'eb1@sil.org stephen_mcconnel@sil.org';
+def packagename = 'Bloom'
+def subdir_name = 'bloom-desktop'
+def subdir_name_beta = 'bloom-desktop-beta'
+def subdir_name_unstable = 'bloom-desktop-unstable'
+def distros_tobuild = 'precise trusty xenial'
+def repo = 'git://github.com/BloomBooks/BloomDesktop.git'
+def email_recipients = 'eb1@sil.org stephen_mcconnel@sil.org'
 
-def revision = "\$(echo \${GIT_COMMIT} | cut -b 1-6)";
-def package_version = '--package-version "\${FULL_BUILD_NUMBER}" ';
+def revision = "\$(echo \${GIT_COMMIT} | cut -b 1-6)"
+def package_version = '--package-version "\${FULL_BUILD_NUMBER}" '
 
 /*
  * Definition of jobs
@@ -30,34 +30,34 @@ def package_version = '--package-version "\${FULL_BUILD_NUMBER}" ';
  */
 freeStyleJob('Bloom_Packaging-Linux-all-3.6-release') {
     common.defaultPackagingJob(delegate, packagename, subdir_name_beta, package_version, revision,
-        distros_tobuild, email_recipients, 'Version3.6');
+        distros_tobuild, email_recipients, 'Version3.6')
 
     description '''
 <p>Nightly builds of the Version3.6 branch.</p>
 <p>The job is created by the DSL plugin from <i>BloomPackagingJobs.groovy</i> script.</p>
-''';
+'''
 
     triggers {
-        githubPush();
+        githubPush()
     }
 
     common.gitScm(delegate, repo, "\$BranchOrTagToBuild",
-        false, subdir_name, false, true);
+        false, subdir_name, false, true)
 }
 
 freeStyleJob('Bloom_Packaging-Linux-all-master-release') {
     common.defaultPackagingJob(delegate, packagename, subdir_name_unstable, package_version, revision,
-        distros_tobuild, email_recipients, 'master');
+        distros_tobuild, email_recipients, 'master')
 
     description '''
 <p>Nightly builds of the Bloom master branch.</p>
 <p>The job is created by the DSL plugin from <i>BloomPackagingJobs.groovy</i> script.</p>
-''';
+'''
 
     triggers {
-        githubPush();
+        githubPush()
     }
 
     common.gitScm(delegate, repo, "\$BranchOrTagToBuild",
-        false, subdir_name_unstable, false, true);
+        false, subdir_name_unstable, false, true)
 }
