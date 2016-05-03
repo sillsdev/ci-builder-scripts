df04334cf8bd5ae51c92a5c44a727fcedd39da14
Eberhard Beilharz <eb1@sil.org>
2016-05-03 16:57:23 +0200

    [lfmerge] Install composer and PHP dependencies
    This is needed for running unit tests that now make use of PHP.
    
    Change-Id: Ia6d3f6f216ab7844b7a925a7c4704da91ca4e878

--- groovy/utilities/LfMerge.groovy
+++ groovy/utilities/LfMerge.groovy
@@ -87,6 +87,24 @@ hg checkout 3.0.1
 make local
 cp -r mercurial ../../Mercurial/''')
 
+				// Install composer and initialize LF php code
+				shell('''#!/bin/bash
+set -e
+cd data/php/src
+if [ ! -f vendor/autoload.php ]; then
+	echo "Installing composer and PHP dependencies"
+	php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
+	php -r "if (hash_file('SHA384', 'composer-setup.php') === 'a52be7b8724e47499b039d53415953cc3d5b459b9d9c0308301f867921c19efc623b81dfef8fc2be194a5cf56945d223') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+	php composer-setup.php
+	php -r "unlink('composer-setup.php');"
+	php composer.phar install
+fi
+if [ ! -f mongo-1.4.1.installed ]; then
+	echo "Installing PECL mongo extension"
+	sudo pecl install mongo-1.4.1
+	touch mongo-1.4.1.installed
+fi
+''')
 				// Compile and run tests
 				shell('''#!/bin/bash
 set -e
