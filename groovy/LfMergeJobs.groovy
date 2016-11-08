103d420f69aeeade82910f42fb7f4f73d9e6fc03
Eberhard Beilharz <eb1@sil.org>
2016-11-08 10:05:30 +0100

    [lfmerge] Clear out previous results before building
    Change-Id: If594869b95e831a9a4ce0057fd051731cf76eee1

--- groovy/LfMergeJobs.groovy
+++ groovy/LfMergeJobs.groovy
@@ -100,6 +100,7 @@ if [ "\$PackageBuildKind" = "Release" ]; then
 fi
 
 mkdir -p finalresults
+rm finalresults/*
 
 cd lfmerge
 # We need to set MONO_PREFIX because that's set to a mono 2.10 installation on the packaging machine!
