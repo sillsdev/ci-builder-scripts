1f0f41e0fadd63dc6b99afddc9b99a9b82893446
Eberhard Beilharz <eb1@sil.org>
2017-03-01 15:22:01 +0100

    [lfmerge] Rename TC agent
    We now use the Xenial build agent on TC, so we should update that
    after building packages.
    
    Change-Id: Ic974b308a56a45da6aa5375c34f11656ba606864

--- groovy/LfMergeJobs.groovy
+++ groovy/LfMergeJobs.groovy
@@ -167,9 +167,9 @@ done
 				shell('''#!/bin/bash
 echo Waiting 5 minutes for package to show up on LLSO
 sleep 300
-ssh ba-trusty64weba sudo apt update || true
-ssh ba-trusty64weba sudo apt -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install -y -f lfmerge || true
-ssh ba-trusty64weba sudo apt -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install -y -f || true''')
+ssh ba-xenial64weba sudo apt update || true
+ssh ba-xenial64weba sudo apt -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install -y -f lfmerge || true
+ssh ba-xenial64weba sudo apt -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install -y -f || true''')
 			}
 		}
 	}
