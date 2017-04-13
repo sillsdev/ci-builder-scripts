8921881566a98e68af18046c3738730ed7d0fa60
Stephen McConnel <stephen_mcconnel@sil.org>
2017-04-13 11:56:04 -0500

    Promote Bloom 3.8 to release and Bloom 3.9 to beta
    Change-Id: If49d85d5b67de2bca0b1606dee1c5c17894e14bc

--- groovy/BloomPackagingJobs.groovy
+++ groovy/BloomPackagingJobs.groovy
@@ -23,8 +23,10 @@ def package_version = '--package-version "\${FULL_BUILD_NUMBER}" '
  */
 
 /*
- * We have three jobs on three different branches for alpha ('master'), beta ('Version3.8'),
- * and release ('Version3.7').
+ * We have four jobs on four different branches for alpha ('master'), beta ('Version3.9'),
+ * and release ('Version3.8' and 'Version3.7').
+ * Version3.7 is the last version which builds for precise, so we're keeping it around at least
+ * for a bit even after precise's impending End Of Life.
  */
 freeStyleJob('Bloom_Packaging-Linux-all-3.7-release') {
 	Common.defaultPackagingJob(delegate, packagename, subdir_name, package_version, revision,
@@ -51,12 +53,37 @@ freeStyleJob('Bloom_Packaging-Linux-all-3.7-release') {
 	}
 }
 
-freeStyleJob('Bloom_Packaging-Linux-all-3.8-beta') {
-	Common.defaultPackagingJob(delegate, packagename, subdir_name_beta, package_version, revision,
+freeStyleJob('Bloom_Packaging-Linux-all-3.8-release') {
+	Common.defaultPackagingJob(delegate, packagename, subdir_name, package_version, revision,
 		distros_tobuild, email_recipients, 'Version3.8')
 
 	description '''
 <p>Automatic ("nightly") builds of the Bloom Version3.8 branch.</p>
+<p>The job is created by the DSL plugin from <i>BloomPackagingJobs.groovy</i> script.</p>
+'''
+
+	triggers {
+		githubPush()
+	}
+
+	Common.gitScm(delegate, repo, "\$BranchOrTagToBuild",
+		false, subdir_name_beta, false, true)
+
+	wrappers {
+		timeout {
+			likelyStuck()
+			abortBuild()
+			writeDescription("Build timed out after {0} minutes")
+		}
+	}
+}
+
+freeStyleJob('Bloom_Packaging-Linux-all-3.9-beta') {
+	Common.defaultPackagingJob(delegate, packagename, subdir_name_beta, package_version, revision,
+		distros_tobuild, email_recipients, 'Version3.9')
+
+	description '''
+<p>Automatic ("nightly") builds of the Bloom Version3.9 branch.</p>
 <p>The job is created by the DSL plugin from <i>BloomPackagingJobs.groovy</i> script.</p>
 '''
 
