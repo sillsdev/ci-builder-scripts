77d71117d29a3ba2bf2c5e5e261898fb3f2056a8
Eberhard Beilharz <eb1@sil.org>
2017-08-25 18:53:51 +0200

    [geckofx] Disable old package build jobs
    Change-Id: I445b00e92f07f0f337fe62dc0cd0357a5aab363f

--- groovy/geckofx_jobs.groovy
+++ groovy/geckofx_jobs.groovy
@@ -4,44 +4,48 @@
 //#include utilities/Common.groovy
 
 // Variables
-def distros_tobuild = 'trusty xenial';
-def revision = "\$(echo \${MERCURIAL_REVISION_SHORT} | cut -b 1-6)";
+def distros_tobuild = 'trusty xenial'
+def revision = "\$(echo \${MERCURIAL_REVISION_SHORT} | cut -b 1-6)"
 
 /*
  * Definition of jobs
  */
 
 freeStyleJob('Geckofx29_NightlyPackaging-Linux-all-master-debug') {
-	Common.defaultPackagingJob(delegate, 'geckofx29', 'geckofx29', "", revision, distros_tobuild);
+	Common.defaultPackagingJob(delegate, 'geckofx29', 'geckofx29', "", revision, distros_tobuild)
 
 	description '''
 <p>Nightly builds of the geckofx-29.0 default branch.</p>
 <p>The job is created by the DSL plugin from <i>geckofx-jobs.groovy</i> script.</p>
-''';
+'''
+
+	disabled()
 
 	triggers {
 		// Check once every day for new changes
 		// Times are UTC
-		scm("H H(4-10) * * *");
+		scm("H H(4-10) * * *")
 	}
 
-	Common.hgScm(delegate, 'https://bitbucket.org/geckofx/geckofx-29.0', 'default', 'geckofx29');
+	Common.hgScm(delegate, 'https://bitbucket.org/geckofx/geckofx-29.0', 'default', 'geckofx29')
 }
 
 freeStyleJob('Geckofx33_NightlyPackaging-Linux-all-master-debug') {
-	Common.defaultPackagingJob(delegate, 'geckofx33', 'geckofx33', "", revision, distros_tobuild);
+	Common.defaultPackagingJob(delegate, 'geckofx33', 'geckofx33', "", revision, distros_tobuild)
 
 	description '''
 <p>Nightly builds of the geckofx-33.0 default branch.</p>
 <p>The job is created by the DSL plugin from <i>geckofx-jobs.groovy</i> script.</p>
-''';
+'''
+
+	disabled()
 
 	triggers {
 		// Check once every day for new changes
 		// Times are UTC
-		scm("H H(4-10) * * *");
+		scm("H H(4-10) * * *")
 	}
 
-	Common.hgScm(delegate, 'https://bitbucket.org/geckofx/geckofx-33.0', 'default', 'geckofx33');
+	Common.hgScm(delegate, 'https://bitbucket.org/geckofx/geckofx-33.0', 'default', 'geckofx33')
 }
 
