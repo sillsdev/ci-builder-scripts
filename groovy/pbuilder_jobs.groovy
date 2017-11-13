51efd4e2319151d4315a0370d8caa56ed4d83b67
Eberhard Beilharz <eb1@sil.org>
2017-11-13 16:17:13 +0100

    Add job to create pbuilder chroots
    This job will have to be triggered manually.
    
    Change-Id: I402ebb520bb6d96d2aa48f54104ba0046cd16e69

--- groovy/pbuilder_jobs.groovy
+++ groovy/pbuilder_jobs.groovy
@@ -2,10 +2,10 @@
  * DSL script for package builder Jenkins jobs
  */
 
-def distros = "trusty xenial jessie"
+def distros = "trusty xenial bionic"
 
 freeStyleJob('PBuilder_Update-Linux-all-master-debug') {
-    description '''
+	description '''
 <p>Maintenance job that updates all chroot instances for pbuilder</p>
 <p>The job is created by the DSL plugin from <i>pbuilder-jobs.groovy</i> script.</p>
 '''
@@ -38,7 +38,7 @@ $HOME/ci-builder-scripts/bash/update --no-package --dists "$Distributions"
 }
 
 freeStyleJob('PBuilder_Cleanup-Linux-all-master-debug') {
-    description '''
+	description '''
 <p>Maintenance job that cleans out previously built binary packages and cancelled builds left on disk
 that are at least two days old.</p>
 <p>The job is created by the DSL plugin from <i>pbuilder-jobs.groovy</i> script.</p>
@@ -62,6 +62,42 @@ that are at least two days old.</p>
 	steps {
 		shell('''
 $HOME/ci-builder-scripts/bash/clean-old-builds --no-package
+''')
+	}
+}
+
+freeStyleJob('PBuilder_Setup-Linux-all-master-debug') {
+	description '''
+<p>Maintenance job that creates chroot instances for pbuilder. To be triggered manually.</p>
+<p>The job is created by the DSL plugin from <i>pbuilder-jobs.groovy</i> script.</p>
+'''
+
+	label 'packager'
+
+	logRotator(365, 100, 10, 10)
+
+	parameters {
+		stringParam("Distributions", distros,
+			"The distributions to create")
+	}
+
+	wrappers {
+		timestamps()
+		colorizeOutput()
+	}
+
+	steps {
+		shell('''
+cd $HOME/ci-builder-scripts/bash
+. common.sh
+init "$@"
+
+for distribution in $Distributions; do
+    for arch in $ARCHES_TO_PACKAGE; do
+		PBUILDERDIR="$pbuilder_path" DISTRIBUTIONS="$distribution" ARCHES="$arch" \
+			$HOME/FwSupportTools/packaging/pbuilder/setup.sh
+    done
+done
 ''')
 	}
 }
