e66583143d995d43af390bcfd17f8e2bf465e15d
Eberhard Beilharz <eb1@sil.org>
2018-04-12 16:28:56 +0200

    Use loop to create library jobs
    Change-Id: I49a053b36389ad2a1166ec1b7fc06609224429ca

--- groovy/LibraryJobs.groovy
+++ groovy/LibraryJobs.groovy
@@ -3,76 +3,28 @@
  * This software is licensed under the MIT license (http://opensource.org/licenses/MIT)
  */
 
-// *********************************************************************************************
-multibranchPipelineJob('icu-dotnet') {
-	description '''<p>Builds of icu-dotnet</p>
-<p>The job is created by the DSL plugin from <i>LibraryJobs.groovy</i> script.</p>'''
+for (repo in ['icu-dotnet', 'icu4c', 'SIL.BuildTasks']) {
+	multibranchPipelineJob(repo) {
+		description """<p>Builds of ${repo}</p>
+	<p>The job is created by the DSL plugin from <i>LibraryJobs.groovy</i> script.</p>"""
 
-	branchSources {
-		github {
-			id('icu-dotnet')
-			repoOwner('sillsdev')
-			repository('icu-dotnet')
-			scanCredentialsId('github-sillsdevgerrit')
-			buildOriginBranch(true)
-			buildOriginBranchWithPR(false)
-			buildOriginPRMerge(true)
-			buildForkPRMerge(true)
-		}
-
-		orphanedItemStrategy {
-			discardOldItems {
-				numToKeep(10)
+		branchSources {
+			github {
+				id(repo)
+				repoOwner('sillsdev')
+				repository(repo)
+				scanCredentialsId('github-sillsdevgerrit')
+				buildOriginBranch(true)
+				buildOriginBranchWithPR(false)
+				buildOriginPRMerge(true)
+				buildForkPRMerge(true)
 			}
-		}
-	}
-}
 
-// *********************************************************************************************
-multibranchPipelineJob('icu4c') {
-	description '''<p>Builds of ICU4C nuget packages</p>
-<p>The job is created by the DSL plugin from <i>LibraryJobs.groovy</i> script.</p>'''
-
-	branchSources {
-		github {
-			id('icu4c')
-			repoOwner('sillsdev')
-			repository('icu4c')
-			scanCredentialsId('github-sillsdevgerrit')
-			buildOriginBranch(true)
-			buildOriginBranchWithPR(false)
-			buildOriginPRMerge(true)
-			buildForkPRMerge(true)
-		}
-
-		orphanedItemStrategy {
-			discardOldItems {
-				numToKeep(10)
-			}
-		}
-	}
-}
-
-// *********************************************************************************************
-multibranchPipelineJob('SIL.BuildTasks') {
-	description '''<p>Builds of SIL.BuildTasks</p>
-<p>The job is created by the DSL plugin from <i>LibraryJobs.groovy</i> script.</p>'''
-
-	branchSources {
-		github {
-			id('SIL.BuildTasks')
-			repoOwner('sillsdev')
-			repository('SIL.BuildTasks')
-			scanCredentialsId('github-sillsdevgerrit')
-			buildOriginBranch(true)
-			buildOriginBranchWithPR(false)
-			buildOriginPRMerge(true)
-			buildForkPRMerge(true)
-		}
-
-		orphanedItemStrategy {
-			discardOldItems {
-				numToKeep(10)
+			orphanedItemStrategy {
+				discardOldItems {
+					daysToKeep(60)
+					numToKeep(10)
+				}
 			}
 		}
 	}
