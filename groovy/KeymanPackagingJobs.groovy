93841dd1c76c18d75e7eef895e17998f68a70f7c
Eberhard Beilharz <eb1@sil.org>
2020-03-02 11:17:10 +0100

    [Keyman] Remove non-pipeline Keyman packaging jobs
    Change-Id: Ibe6bcdf2cac25468d04daaa5e0a3309c2a82896a

--- groovy/KeymanPackagingJobs.groovy
+++ groovy/KeymanPackagingJobs.groovy
@@ -1,17 +1,18 @@
 /*
- * Copyright (c) 2018 SIL International
+ * Copyright (c) 2018-2020 SIL International
  * This software is licensed under the MIT license (http://opensource.org/licenses/MIT)
  */
 //#include utilities/Common.groovy
 
 // Variables
-final stable_branch = 'stable-13.0'
 final distros_tobuild = "xenial bionic"
-final repo = 'git://github.com/keymanapp/keyman.git'
 final email_recipients = 'eb1@sil.org marc_durdin@sil.org dglassey@gmail.com darcy_wong@sil.org'
 
 final revision = "\$(echo \${GIT_COMMIT} | cut -b 1-6)"
 final fullBuildNumber="0.0.0+\$BUILD_NUMBER"
+
+final onboard_repo = 'git://github.com/keymanapp/onboard-keyman.git'
+final branch = 'keymankb'
 
 /*
  * Definition of jobs
@@ -20,180 +21,23 @@ final fullBuildNumber="0.0.0+\$BUILD_NUMBER"
  * and commit and push the changes.
  */
 
-
-// We have multiple jobs on different branches for release, beta ('master') and alpha ('develop')
-for (branch in [stable_branch, 'beta', 'master']) {
-	for (buildKind in ['regular' /*, 'pr'*/]) {
-		switch (buildKind) {
-			case 'regular':
-				jobinfix = ''
-				break
-			case 'pr':
-				jobinfix = '-PR'
-				break
-		}
-		// We are building multiple packages in this job
-		for (packagebasename in ['keyman-keyboardprocessor', 'kmflcomp', 'libkmfl', 'ibus-kmfl', 'keyman-config', 'ibus-keyman']) {
-			if (packagebasename == 'keyman-keyboardprocessor') {
-				subdir_name = "common/engine/keyboardprocessor"
-			} else {
-				subdir_name = "linux/${packagebasename}"
-			}
-			build_distros = 'xenial bionic'
-			baseExtraParameter = "--nightly-delimiter '~' --source-code-subdir ${subdir_name}"
-			package_version = """--package-version "${fullBuildNumber}" """
-
-			packagename = "${packagebasename}"
-			extraParameter = "${baseExtraParameter}"
-			switch (branch) {
-				case stable_branch:
-					kind = 'stable'
-					break
-				case 'beta':
-					packagename = "${packagebasename}-beta"
-					kind = 'beta'
-					extraParameter = "${baseExtraParameter} --append-to-package -beta"
-					break
-				case 'master':
-					packagename = "${packagebasename}-alpha"
-					kind = 'alpha'
-					extraParameter = "${baseExtraParameter} --append-to-package -alpha"
-					break
-			}
-
-
-			freeStyleJob("Keyman_Packaging${jobinfix}-Linux-${packagebasename}-${kind}") {
-
-				mainRepoDir = '.'
-
-				Common.defaultPackagingJob(delegate, packagename, subdir_name, package_version, revision,
-					build_distros, email_recipients, branch, "amd64 i386", build_distros, true, mainRepoDir,
-					/* buildMasterBranch: */ false, /* addParameters */ true, /* addSteps */ false,
-					/* resultsDir: */ "results", /* extraSourceArgs: */ extraParameter,
-					/* extraBuildArgs: */ '', /* fullBuildNumber: */ fullBuildNumber,
-					/* nodeLabel: */ 'packager && bionic')
-
-				description """
-<p>Automatic builds of the Keyman for Linux ${branch} branch.</p>
-<p>The job is created by the DSL plugin from <i>KeymanPackagingJobs.groovy</i> script.</p>
-"""
-
-				// TriggerToken needs to be set in the seed job! Note that we use the
-				// `binding.variables.*` notation so that it works when we build the tests.
-				authenticationToken(binding.variables.TriggerToken)
-
-				if (packagename == 'keyman-keyboardprocessor') {
-					onlyTriggerFileSpec = "common/engine/.*\nTIER.md\nVERSION.md"
-				} else {
-					onlyTriggerFileSpec = "linux/.*\ncommon/engine/.*\nresources/.*\nTIER.md\nVERSION.md"
-				}
-
-				triggers {
-					if (buildKind == 'regular') {
-						extraPackageArgs = ''
-						githubPush()
-					} else {
-						extraPackageArgs = '--no-upload'
-						githubPullRequest {
-							admin('ermshiperete')
-							useGitHubHooks(true)
-							userWhitelist('')
-							orgWhitelist('keymanapp')
-							allowMembersOfWhitelistedOrgsAsAdmin(true)
-							displayBuildErrorsOnDownstreamBuilds(true)
-							cron('H/5 * * * *')
-							whiteListTargetBranches([ branch ])
-							includedRegions(onlyTriggerFileSpec)
-						}
-					}
-				}
-
-				Common.gitScm(delegate, /*url*/ repo, /*branch*/"\$BranchOrTagToBuild",
-					/*createTag*/ false, /*subdir*/ "", /*disableSubmodules*/ false,
-					/*commitAuthorInChangelog*/ true, /*scmName*/ "", /*refspec*/ "",
-					/*clean*/ true, /*credentials*/ "", /*fetchTags*/ true,
-					/*onlyTriggerFileSpec*/ onlyTriggerFileSpec,
-					/*githubRepo*/ "keymanapp/keyman")
-
-				wrappers {
-					timeout {
-						elastic(300, 3, 120)
-						abortBuild()
-						writeDescription("Build timed out after {0} minutes")
-					}
-				}
-
-				steps {
-					shell("""#!/bin/bash
-export FULL_BUILD_NUMBER=${fullBuildNumber}
-
-if [ "\$PackageBuildKind" = "Release" ]; then
-	MAKE_SOURCE_ARGS="--preserve-changelog"
-	BUILD_PACKAGE_ARGS="--suite-name main"
-elif [ "\$PackageBuildKind" = "ReleaseCandidate" ]; then
-	MAKE_SOURCE_ARGS="--preserve-changelog"
-	BUILD_PACKAGE_ARGS="--suite-name proposed"
-fi
-
-rm -f ${packagename}-packageversion.properties
-basedir=`pwd`
-
-# set Debian/changelog environment
-export DEBEMAIL='jenkins@sil.org'
-
-# make source package
-cd linux
-./scripts/jenkins.sh ${packagename} \$DEBSIGNKEY
-cd ..
-cd ${subdir_name}
-
-\$HOME/ci-builder-scripts/bash/build-package --dists "\$DistributionsToPackage" \
-	--arches "\$ArchesToPackage" \
-	--main-package-name "${packagename}" \
-	--supported-distros "${distros_tobuild}" \
-	--debkeyid \$DEBSIGNKEY \
-	--build-in-place \
-	\$BUILD_PACKAGE_ARGS ${extraPackageArgs}
-
-buildret="\$?"
-
-if [ "\$buildret" == "0" ]; then echo "PackageVersion=\$(for file in `ls -1 ${packagebasename}*_source.build`;do basename \$file _source.build;done|cut -d "_" -f2|cut -d "-" -f1)" > \$basedir/${packagename}-packageversion.properties; fi
-exit \$buildret
-""")
-
-					environmentVariables {
-						propertiesFile("${packagename}-packageversion.properties")
-					}
-
-					Common.addBuildNumber(delegate, 'PackageVersion')
-				} /*steps*/
-			} /*freeStyleJob*/
-		} /*branch*/
-	} /*buildKind*/
-} /*job*/
-
-// Job to build onboard-keyman
-
-def onboard_repo = 'git://github.com/keymanapp/onboard-keyman.git'
-branch = 'keymankb'
-
 freeStyleJob("Keyman_Packaging-Linux-onboard-keyman-${branch}") {
 	mainRepoDir = '.'
 	packagename = "onboard"
-	Common.defaultPackagingJob(delegate, "onboard-keyman", "", package_version, revision,
-			distros_tobuild, email_recipients, branch, "amd64 i386", "xenial bionic", true, mainRepoDir,
-			/* buildMasterBranch: */ false, /* addParameters */ true, /* addSteps */ false,
-			/* resultsDir: */ "results", /* extraSourceArgs: */ extraParameter,
-			/* extraBuildArgs: */ '', /* fullBuildNumber: */ fullBuildNumber,
-			/* nodeLabel: */ 'packager && bionic')
+	Common.defaultPackagingJob(delegate, "onboard-keyman", "", "", revision,
+		distros_tobuild, email_recipients, branch, "amd64 i386", distros_tobuild, true, mainRepoDir,
+		/* buildMasterBranch: */ false, /* addParameters */ true, /* addSteps */ false,
+		/* resultsDir: */ "results", /* extraSourceArgs: */ "",
+		/* extraBuildArgs: */ '', /* fullBuildNumber: */ fullBuildNumber,
+		/* nodeLabel: */ 'packager && bionic')
 
 	description """
 <p>Automatic builds of the Onboard Keyboard for Linux ${branch} branch.</p>
 <p>The job is created by the DSL plugin from <i>KeymanPackagingJobs.groovy</i> script.</p>
 """
 
-		// TriggerToken needs to be set in the seed job! Note that we use the
-		// `binding.variables.*` notation so that it works when we build the tests.
+	// TriggerToken needs to be set in the seed job! Note that we use the
+	// `binding.variables.*` notation so that it works when we build the tests.
 	authenticationToken(binding.variables.TriggerToken)
 
 	triggers {
--- groovy/KeymanViews.groovy
+++ groovy/KeymanViews.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 SIL International
+ * Copyright (c) 2018-2020 SIL International
  * This software is licensed under the MIT license (http://opensource.org/licenses/MIT)
  */
 /*
@@ -38,42 +38,6 @@ class keymanViews {
 		}
 	}
 
-	static void addPRPackageBuildsListView(viewContext) {
-		viewContext.with {
-			categorizedJobsView('PR package builds') {
-				description "Pre-merge package builds of <b>Keyman</b>"
-				filterBuildQueue false
-				filterExecutors false
-
-				jobs {
-					regex('^Keyman_Packaging-PR-Linux.*')
-				}
-
-				columns {
-					status()
-					weather()
-					name()
-					lastSuccess()
-					lastFailure()
-					lastDuration()
-					lastBuildTriggerColumn {
-						causeDisplayType("icon")
-					}
-					buildButton()
-					lastBuildNode()
-					lastBuildConsole()
-					slaveOrLabel()
-				}
-
-				categorizationCriteria {
-					regexGroupingRule('^Keyman_Packaging-PR-Linux.*-alpha', 'PR Alpha Package builds')
-					regexGroupingRule('^Keyman_Packaging-PR-Linux.*-beta', 'PR Beta Package builds')
-					regexGroupingRule('^Keyman_Packaging-PR-Linux.*-stable', 'PR Stable Package builds')
-				}
-			}
-		}
-	}
-
 	static void addKeymanPipelineJobs(viewContext) {
 		viewContext.with {
 			categorizedJobsView('Pipeline') {
@@ -104,15 +68,10 @@ class keymanViews {
 
 nestedView('Keyman') {
 	configure { view ->
-		view / defaultView('All')
+		view / defaultView('Pipeline')
 	}
 	views {
-		keymanViews.KeymanViewAll(delegate)
-		CommonViews.addPackageBuildsListView(delegate, 'Keyman', '^Keyman_Packaging-Linux.*-alpha', 'Alpha Package builds')
-		CommonViews.addPackageBuildsListView(delegate, 'Keyman', '^Keyman_Packaging-Linux.*-beta', 'Beta Package builds')
-		CommonViews.addPackageBuildsListView(delegate, 'Keyman', '^Keyman_Packaging-Linux.*-stable', 'Stable Package builds')
-		CommonViews.addPackageBuildsListView(delegate, 'Keyman', '^Keyman_Packaging-Linux-onboard-keyman-.*', 'OnboardKeyboard Package builds')
-		keymanViews.addPRPackageBuildsListView(delegate)
 		keymanViews.addKeymanPipelineJobs(delegate)
+		CommonViews.addPackageBuildsListView(delegate, 'Keyman', '^Keyman_Packaging-Linux-onboard-keyman-.*', 'OnboardKeyboard Package builds')
 	}
 }
