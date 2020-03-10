d485586c21695d8123b827f75d71faa73a5bf565
MarkS <marksvc@gmail.com>
2020-03-10 16:39:42 -0600

    [flexbridge] Remove trusty, devel
    * Build system doesn't appear to be configured for those.
    
    Change-Id: I9ec732a4acabba9d02d4ac07c7b82c1c0021f83b

--- groovy/FLExBridgeJobs.groovy
+++ groovy/FLExBridgeJobs.groovy
@@ -6,7 +6,7 @@
 
 // Variables
 def packagename = 'flexbridge'
-def distros = 'focal bionic xenial trusty devel'
+def distros = 'focal bionic xenial'
 def repo = 'git://github.com/sillsdev/flexbridge.git'
 def email_recipients = 'eb1@sil.org'
 def subdir_name = 'flexbridge'
