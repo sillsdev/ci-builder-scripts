e07f877bea7ae1fc76104599cc61ac25331ef9f7
Eberhard Beilharz <eb1@sil.org>
2020-08-18 15:09:09 +0200

    [Packaging] Re-add pbuilder jobs
    Some packaging jobs still user pbuilder, so we still need the script
    to create and update new pbuilder chroots.
    
    Change-Id: I542f1b5bb87addccfeca144c31bd0f1ebe30fd37

---
+++ groovy/pbuilder_jobs.groovy
@@ -0,0 +1,41 @@
+/*
+ * DSL script for package builder Jenkins jobs
+ */
+
+def distros = "xenial bionic focal"
+
+pipelineJob('PBuilder_Setup-Linux-all') {
+	description '''
+<p>Maintenance job that creates chroot instances for pbuilder. To be triggered manually.</p>
+<p>The job is created by the DSL plugin from <i>pbuilder-jobs.groovy</i> script.</p>
+'''
+
+	logRotator(365, 100, 10, 10)
+
+	parameters {
+		stringParam("Distributions", distros,
+			"The distributions to create")
+	}
+
+	definition {
+		cps {
+			script('''@Library('lsdev-pipeline-library') _
+				runOnAllNodes(label: 'packager',
+					command: \'\'\'cd $HOME/ci-builder-scripts/bash
+. ./common.sh
+init "$@"
+
+for distribution in $Distributions; do
+	for arch in $ARCHES_TO_PACKAGE; do
+		PBUILDERDIR="$pbuilder_path" DISTRIBUTIONS="$distribution" ARCHES="$arch" \
+			$HOME/FwSupportTools/packaging/pbuilder/setup.sh
+	done
+done
+
+PBUILDERDIR="$pbuilder_path" DISTRIBUTIONS="$distributions" ARCHES="$ARCHES_TO_PACKAGE" $HOME/FwSupportTools/packaging/pbuilder/setup.sh --update
+\'\'\')
+'''
+			)
+		}
+	}
+}
