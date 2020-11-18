4d92c45aef2c5bca0a96d7a281cab5de62bc8db0
Eberhard Beilharz <eb1@sil.org>
2020-11-18 09:40:09 +0100

    Update mk-sbuild version for Groovy
    Change-Id: I13cf0eecf4ec9c8777699ef675280e1e1742e66c

--- bash/common.sh
+++ bash/common.sh
@@ -49,8 +49,8 @@ general_init()
 	# The version of mk-sbuild we use. We need a current version of that script because trying
 	# to build newer dists might have different requirements than the system provided version
 	# provides.
-	# 0.176 is the version included in Focal
-	MKSBUILD_VERSION=0.176
+	# 0.177 is the version included in Groovy
+	MKSBUILD_VERSION=0.177
 
 	RED='\033[0;31m'
 	GREEN='\033[0;32m'
