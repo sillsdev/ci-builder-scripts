5f50cfbf2cb4f4fdd0da48dc8c0bb972e7de8863
Eberhard Beilharz <eb1@sil.org>
2021-11-08 19:56:06 +0100

    Add reboot-agents job
    Change-Id: I6820fcd69cd190697083761872dd39dc7866f3e3

---
+++ groovy/reboot_agents.groovy
@@ -0,0 +1,26 @@
+/*
+ * DSL script for package builder Jenkins jobs
+ */
+
+def agents = "autopackager-1 autopackager-2 autopackager-4"
+
+pipelineJob('reboot-agents') {
+	description '''
+<p>Reboot packaging agents once a day.</p>
+<p>The job is created by the DSL plugin from <i>reboot_agents.groovy</i> script.</p>
+'''
+
+	logRotator(365, 100, 10, 10)
+
+	triggers {
+		cron('@daily')
+	}
+
+	definition {
+		cps {
+			script("""@Library('lsdev-pipeline-library') _
+				rebootAgents(label: '${agents}')"""
+			)
+		}
+	}
+}
