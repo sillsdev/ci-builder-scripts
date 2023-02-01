f446f6d0662b195aba53c899f945525e44f171e9
Eberhard Beilharz <eb1@sil.org>
2023-02-01 15:52:35 +0100

    [scripts] Remove shellcheck warnings from setup.sh
    Change-Id: Ief612a278175364240b72b0eda565d0253917f44

--- bash/setup.sh
+++ bash/setup.sh
@@ -7,7 +7,7 @@ set -e -o pipefail
 
 PROGRAM=$(readlink -f "$0")
 PROGRAM_NAME="$(basename "$0")"
-PROGRAM_DIR="$(realpath $(dirname "$0"))"
+PROGRAM_DIR="$(realpath "$(dirname "$0")")"
 
 function helpScript() {
 	cat << EOF
@@ -29,27 +29,26 @@ EOF
 }
 
 . "${PROGRAM_DIR}"/common.sh
-no_package=true
 init "$@"
 
 function isUnknownDistro()
 {
-	! ubuntu-distro-info --series=$1 >/dev/null 2>&1 && ! debian-distro-info --series=$1 >/dev/null 2>&1
+	! ubuntu-distro-info --series="$1" >/dev/null 2>&1 && ! debian-distro-info --series="$1" >/dev/null 2>&1
 }
 
 function isUbuntu()
 {
-	ubuntu-distro-info --series=$1 >/dev/null 2>&1
+	ubuntu-distro-info --series="$1" >/dev/null 2>&1
 }
 
 function isDebian()
 {
-	debian-distro-info --series=$1 >/dev/null 2>&1
+	debian-distro-info --series="$1" >/dev/null 2>&1
 }
 
 function isSupported()
 {
-	if [[ "$(ubuntu-distro-info --supported) $(debian-distro-info --supported)" =~ "$1" ]]; then
+	if [[ "$(ubuntu-distro-info --supported) $(debian-distro-info --supported)" =~ $1 ]]; then
 		return 0
 	fi
 	return 1
@@ -57,13 +56,13 @@ function isSupported()
 
 function checkOrLinkDebootstrapScript()
 {
-	if [ ! -f /usr/share/debootstrap/scripts/$1 ]; then
-		if isUbuntu $1; then
+	if [ ! -f /usr/share/debootstrap/scripts/"$1" ]; then
+		if isUbuntu "$1"; then
 			basedistro=gutsy
 		else
 			basedistro=sid
 		fi
-		sudo ln -s /usr/share/debootstrap/scripts/$basedistro /usr/share/debootstrap/scripts/$1
+		sudo ln -s /usr/share/debootstrap/scripts/$basedistro /usr/share/debootstrap/scripts/"$1"
 	fi
 }
 
@@ -75,14 +74,14 @@ function addmirror()
 # Create a source.list template file that will be used in the chroot
 function createSources()
 {
-	cat > $1 <<EOF
-$(echo $OTHERMIRROR | tr '|' '\n')
+	cat > "$1" <<EOF
+$(echo "$OTHERMIRROR" | tr '|' '\n')
 EOF
 }
 
 function enableBackports()
 {
-	cat > $1 <<EOF
+	cat > "$1" <<EOF
 Package: *
 Pin: release a=$2-backports
 Pin-Priority: 500
@@ -97,56 +96,57 @@ function checkAndInstallRequirements()
 	fi
 	if [ ! -x /usr/bin/sbuild ]; then
 		TOINSTALL="$TOINSTALL sbuild"
-		touch $HOME/.sbuildrc
-	elif [ ! -f $HOME/.sbuildrc ]; then
-		touch $HOME/.sbuildrc
+		touch "$HOME/.sbuildrc"
+	elif [ ! -f "$HOME/.sbuildrc" ]; then
+		touch "$HOME/.sbuildrc"
 	fi
 
 	if [ -n "$TOINSTALL" ]; then
 		log "Installing prerequisites: $TOINSTALL"
 		sudo apt-get update
-		sudo apt-get -qy install $TOINSTALL
+		sudo apt-get -qy install "$TOINSTALL"
 	fi
 
 	GROUP=$(groups | grep sbuild || true)
 	if [ -z "$GROUP" ]; then
-		sudo adduser $USER sbuild > /dev/null || true
-		local TEMPFILE=$(mktemp)
+		sudo adduser "$USER" sbuild > /dev/null || true
+		local TEMPFILE
 		local ARGS=()
+		TEMPFILE=$(mktemp)
 		while (( $# )); do
 			ARGS+=("\"$1\"")
 			shift
 		done
-		echo "${ARGS[@]}" > $TEMPFILE
-		chmod +x $TEMPFILE
+		echo "${ARGS[@]}" > "$TEMPFILE"
+		chmod +x "$TEMPFILE"
 
-		log "Calling $(basename ${ARGS[0]}) recursively"
-		sg sbuild $TEMPFILE
-		rm $TEMPFILE
+		log "Calling $(basename "${ARGS[0]}") recursively"
+		sg sbuild "$TEMPFILE"
+		rm "$TEMPFILE"
 		exit
 	fi
 
 	# We have to install a current version of mk-sbuild because trying to build newer dists
 	# on an older dist might have different requirements than the system provided version
 	# of mk-sbuild provides (e.g. on xenial when trying to build a bionic chroot).
-	if [ ! -f $HOME/bin/mk-sbuild -o ! -f $HOME/bin/mk-sbuild.v${MKSBUILD_VERSION} ]; then
+	if [ ! -f "$HOME/bin/mk-sbuild" ] || [ ! -f "$HOME/bin/mk-sbuild.v${MKSBUILD_VERSION}" ]; then
 		log "Getting version ${MKSBUILD_VERSION} of mk-sbuild"
-		mkdir -p $HOME/bin
-		rm -f $HOME/bin/mk-sbuild*
-		TRACE wget --output-document=$HOME/bin/mk-sbuild https://git.launchpad.net/ubuntu-dev-tools/plain/mk-sbuild?h=${MKSBUILD_VERSION}
-		chmod +x $HOME/bin/mk-sbuild
-		touch $HOME/bin/mk-sbuild.v${MKSBUILD_VERSION}
+		mkdir -p "$HOME/bin"
+		rm -f "$HOME"/bin/mk-sbuild*
+		TRACE wget --output-document="$HOME/bin/mk-sbuild" https://git.launchpad.net/ubuntu-dev-tools/plain/mk-sbuild?h="${MKSBUILD_VERSION}"
+		chmod +x "$HOME/bin/mk-sbuild"
+		touch "$HOME/bin/mk-sbuild.v${MKSBUILD_VERSION}"
 	fi
 
 	# We also need a fairly recent version of debootstrap - the version provided in Xenial is
 	# too old
-	if [ ! -f $HOME/bin/debootstrap.v${DEBOOTSTRAP_VERSION} ]; then
+	if [ ! -f "$HOME/bin/debootstrap.v${DEBOOTSTRAP_VERSION}" ]; then
 		log "Installing version ${DEBOOTSTRAP_VERSION} of debootstrap"
 		pushd /tmp
-		TRACE wget http://mirrors.kernel.org/ubuntu/pool/main/d/debootstrap/debootstrap_${DEBOOTSTRAP_VERSION}_all.deb
-		sudo dpkg -i debootstrap_${DEBOOTSTRAP_VERSION}_all.deb
+		TRACE wget "http://mirrors.kernel.org/ubuntu/pool/main/d/debootstrap/debootstrap_${DEBOOTSTRAP_VERSION}_all.deb"
+		sudo dpkg -i "debootstrap_${DEBOOTSTRAP_VERSION}_all.deb"
 		popd
-		touch $HOME/bin/debootstrap.v${DEBOOTSTRAP_VERSION}
+		touch "$HOME/bin/debootstrap.v${DEBOOTSTRAP_VERSION}"
 	fi
 
 	installFileSbuildrc
@@ -160,7 +160,7 @@ function installFileSbuildrc()
 	fi
 	log "Installing .sbuildrc"
 	# Temporarily backup any existing .sbuildrc, in case we are overwriting something good.
-	cp -v "${SBUILDRC_PATH}" $(mktemp /tmp/old-sbuildrc.XXXXXXXXXX) || true
+	cp -v "${SBUILDRC_PATH}" "$(mktemp /tmp/old-sbuildrc.XXXXXXXXXX)" || true
 	cp -v "${PROGRAM_DIR}"/sbuildrc "${SBUILDRC_PATH}"
 }
 
@@ -189,8 +189,8 @@ function doesChrootExist()
 	local dist=$1
 	local arch=$2
 
-	sudo schroot --list | grep -q source:${dist}-${arch} && \
-		sudo schroot --list | grep -q chroot:${dist}-${arch}
+	sudo schroot --list | grep -q "source:${dist}-${arch}" && \
+		sudo schroot --list | grep -q "chroot:${dist}-${arch}"
 }
 
 function downloadAndExportKey()
@@ -199,10 +199,10 @@ function downloadAndExportKey()
 	local fingerprint=$2
 	local tmp_keyring
 
-	if [ ! -f ${keyfile} ]; then
+	if [ ! -f "${keyfile}" ]; then
 		tmp_keyring="$(mktemp)"
 		gpg --trust-model always --no-default-keyring --keyring "${tmp_keyring}" \
-			--keyserver keyserver.ubuntu.com --recv-keys ${fingerprint}
+			--keyserver keyserver.ubuntu.com --recv-keys "${fingerprint}"
 		gpg --no-default-keyring --keyring "${tmp_keyring}" --export > "${keyfile}"
 		rm -f "${tmp_keyring}"
 	fi
@@ -213,7 +213,7 @@ WORKDIR="${WORKSPACE:-${PROGRAM_DIR}}"
 
 cd "${WORKDIR}"
 
-checkAndInstallRequirements $PROGRAM "$@"
+checkAndInstallRequirements "$PROGRAM" "$@"
 
 KEYRINGLLSO="$WORKDIR/llso-keyring-2013.gpg"
 KEYRINGPSO="$WORKDIR/pso-keyring-2016.gpg"
@@ -222,12 +222,12 @@ KEYRINGMICROSOFT="$WORKDIR/microsoft.asc.gpg"
 KEYRING_MONO="$WORKDIR/mono-project.asc.gpg"
 KEYRING_MICROSOFTPROD="$WORKDIR/microsoft-prod.asc.gpg"
 
-if [ ! -f ${KEYRINGPSO} ]; then
-	wget --output-document=${KEYRINGPSO} https://packages.sil.org/keys/pso-keyring-2016.gpg
+if [ ! -f "${KEYRINGPSO}" ]; then
+	wget --output-document="${KEYRINGPSO}" https://packages.sil.org/keys/pso-keyring-2016.gpg
 fi
 
-if [ ! -f ${KEYRINGLLSO} ]; then
-	wget --output-document=${KEYRINGLLSO} http://linux.lsdev.sil.org/keys/llso-keyring-2013.gpg
+if [ ! -f "${KEYRINGLLSO}" ]; then
+	wget --output-document="${KEYRINGLLSO}" http://linux.lsdev.sil.org/keys/llso-keyring-2013.gpg
 fi
 
 if [ ! -f ${KEYRINGNODE} ]; then
@@ -237,8 +237,8 @@ if [ ! -f ${KEYRINGNODE} ]; then
 	sudo rm ${KEYRINGNODE}~
 fi
 
-if [ ! -f ${KEYRINGMICROSOFT} ]; then
-	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o ${KEYRINGMICROSOFT}
+if [ ! -f "${KEYRINGMICROSOFT}" ]; then
+	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o "${KEYRINGMICROSOFT}"
 fi
 
 downloadAndExportKey "${KEYRING_MONO}" "3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
@@ -248,17 +248,17 @@ for D in ${dists_arg:-$(ubuntu-distro-info --supported) $(debian-distro-info --t
 do
 	for A in ${arches_arg:-$ARCHES_TO_PROCESS}
 	do
-		if isUnknownDistro $D; then
+		if isUnknownDistro "$D"; then
 			echo "Unknown distro $D. Maybe you'll have to update /usr/share/distro-info/ubuntu.csv."
 			continue
 		fi
 
-		doesChrootExist $D $A && [ -z "$update" ] && echo "$D-$A already exists - skipping creation" && continue
-		! doesChrootExist $D $A && [ -n "$update" ] && echo "$D-$A doesn't exist - skipping update" && continue
+		doesChrootExist "$D" "$A" && [ -z "$update" ] && echo "$D-$A already exists - skipping creation" && continue
+		! doesChrootExist "$D" "$A" && [ -n "$update" ] && echo "$D-$A doesn't exist - skipping update" && continue
 
 		if [ "$A" == "i386" ]; then
 			# Starting with Ubuntu 21.10 (Impish) there is only 64-bit available
-			if (( $(ubuntu-distro-info --series=$D -r|cut -d'.' -f1) >= 21 )); then
+			if (( $(ubuntu-distro-info --series="$D" -r|cut -d'.' -f1) >= 21 )); then
 				log "Skipping 32bit chroot for $D"
 				continue
 			fi
@@ -266,33 +266,33 @@ do
 
 		log "Processing $D-$A"
 
-		if ! doesChrootExist $D $A; then
+		if ! doesChrootExist "$D" "$A"; then
 			# Remove remnants of failed build
-			[ -e $SCHROOTDIR/$D-$A ] && sudo rm -rf $SCHROOTDIR/$D-$A
+			[ -e "$SCHROOTDIR/$D-$A" ] && sudo rm -rf "$SCHROOTDIR/$D-$A"
 		fi
 
 		OTHERMIRROR=""
 
-		checkOrLinkDebootstrapScript $D
+		checkOrLinkDebootstrapScript "$D"
 
-		if isUbuntu $D; then
+		if isUbuntu "$D"; then
 			DISTRO=ubuntu
-			LTSDIST=$(ubuntu-distro-info --lts)
+			# LTSDIST=$(ubuntu-distro-info --lts)
 			# packages.microsoft is a 64-bit only repo. 32-bit can be downloaded as a tar.
 			if [ "$D" == "$(ubuntu-distro-info --devel)" ]; then
-				MICROSOFT_APT="deb [arch=amd64] https://packages.microsoft.com/ubuntu/$(ubuntu-distro-info --series=${UBUNTU_LAST_RELEASE_MICROSOFT} -r | cut -d' ' -f1)/prod ${UBUNTU_LAST_RELEASE_MICROSOFT} main"
+				MICROSOFT_APT="deb [arch=amd64] https://packages.microsoft.com/ubuntu/$(ubuntu-distro-info --series="${UBUNTU_LAST_RELEASE_MICROSOFT}" -r | cut -d' ' -f1)/prod ${UBUNTU_LAST_RELEASE_MICROSOFT} main"
 			else
-				MICROSOFT_APT="deb [arch=amd64] https://packages.microsoft.com/ubuntu/$(ubuntu-distro-info --series=${D} -r | cut -d' ' -f1)/prod ${D} main"
+				MICROSOFT_APT="deb [arch=amd64] https://packages.microsoft.com/ubuntu/$(ubuntu-distro-info --series="${D}" -r | cut -d' ' -f1)/prod ${D} main"
 			fi
-			if (( $(ubuntu-distro-info --series=$D -r|cut -d'.' -f1) >= 20 )); then
+			if (( $(ubuntu-distro-info --series="$D" -r|cut -d'.' -f1) >= 20 )); then
 				MONO_APT="deb http://download.mono-project.com/repo/ubuntu stable-focal main"
-			elif (( $(ubuntu-distro-info --series=$D -r|cut -d'.' -f1) >= 18 )); then
+			elif (( $(ubuntu-distro-info --series="$D" -r|cut -d'.' -f1) >= 18 )); then
 				MONO_APT="deb http://download.mono-project.com/repo/ubuntu stable-bionic main"
 			else
 				MONO_APT="deb http://download.mono-project.com/repo/ubuntu stable-xenial main"
 			fi
 
-			if isSupported $D; then
+			if isSupported "$D"; then
 				MIRROR="${UBUNTU_MIRROR:-http://archive.ubuntu.com/ubuntu/}"
 			else
 				MIRROR="${UBUNTU_OLDMIRROR:-http://old-releases.ubuntu.com/ubuntu/}"
@@ -315,12 +315,12 @@ do
 
 			# Allow to install current nodejs packages
 			if [ -n "$update" ]; then
-				if [ "$D" == "bionic" -o "$D" == "focal" ]; then
+				if [ "$D" == "bionic" ] || [ "$D" == "focal" ]; then
 					# Starting with Groovy (Ubuntu 20.10) Node 12 is available as Debian packages
 					addmirror "deb https://deb.nodesource.com/node_12.x $D main"
 				fi
 			fi
-		elif isDebian $D; then
+		elif isDebian "$D"; then
 			DISTRO=debian
 			# packages.microsoft is a 64-bit only repo. 32-bit can be downloaded as a tar.
 			MICROSOFT_APT="deb [arch=amd64] http://packages.microsoft.com/repos/microsoft-debian-${D}-prod ${D} main"
@@ -328,7 +328,7 @@ do
 
 			MIRROR="${DEBIAN_MIRROR:-http://ftp.ca.debian.org/debian/}"
 			COMPONENTS="main contrib non-free"
-			KEYRINGMAIN="/usr/share/keyrings/debian-archive-keyring.gpg"
+			# KEYRINGMAIN="/usr/share/keyrings/debian-archive-keyring.gpg"
 			PROXY="$http_proxy"
 			LLSO="http://linux.lsdev.sil.org/debian/"
 			PSO="http://packages.sil.org/debian/"
@@ -353,38 +353,38 @@ do
 			log "Create new chroot for $D-$A"
 
 			# Build chroot - if that fails remove it
-			TRACE $HOME/bin/mk-sbuild --distro $DISTRO $D --arch=$A \
+			TRACE "$HOME/bin/mk-sbuild "--distro $DISTRO "$D" --arch="$A" \
 				--debootstrap-include="perl,gnupg,debhelper,ca-certificates" \
-				${PROXY:+--debootstrap-proxy=}$PROXY \
-				$OTHEROPTS --type=directory || (sudo rm -rf $SCHROOTDIR/$D-$A; continue)
+				${PROXY:+--debootstrap-proxy=}"$PROXY" \
+				"$OTHEROPTS" --type=directory || (sudo rm -rf "$SCHROOTDIR/$D-$A"; continue)
 
 			copyInKeyrings
 			addExtraRepositories
 
 			TMPFILE=$(mktemp)
-			enableBackports $TMPFILE $D
-			sudo mkdir -p $SCHROOTDIR/$D-$A/etc/apt/preferences.d
-			sudo cp $TMPFILE $SCHROOTDIR/$D-$A/etc/apt/preferences.d/backports
-			rm $TMPFILE
+			enableBackports "$TMPFILE" "$D"
+			sudo mkdir -p "$SCHROOTDIR/$D-$A/etc/apt/preferences.d"
+			sudo cp "$TMPFILE" "$SCHROOTDIR/$D-$A/etc/apt/preferences.d/backports"
+			rm "$TMPFILE"
 
 			PKGLIST="apt-utils devscripts lsb-release apt-transport-https tzdata"
 
 			log "Install packages in chroot for $D-$A"
 			if [ "$(lsb_release --codename --short)" == "xenial" ]; then
 				# Xenial has an older sbuild version that has a buggy sbuild-apt
-				TRACE sudo schroot -c source:$D-$A -u root --directory=/ -- sh -c \
+				TRACE sudo schroot -c "source:$D-$A" -u root --directory=/ -- sh -c \
 					"apt-get -qq update && \
 					apt-get -qy upgrade && \
 					apt-get -qy install $PKGLIST && \
 					apt-get clean" < /dev/null
 			else
 				if [ "$D" == "xenial" ]; then
-					TRACE sudo sbuild-apt $D-$A apt-get install apt-transport-https ca-certificates
+					TRACE sudo sbuild-apt "$D-$A" apt-get install apt-transport-https ca-certificates
 				fi
 
-				TRACE sudo sbuild-update --update --dist-upgrade --upgrade $D-$A
-				TRACE sudo sbuild-apt $D-$A apt-get install $PKGLIST
-				TRACE sudo sbuild-update --clean --autoclean --autoremove $D-$A
+				TRACE sudo sbuild-update --update --dist-upgrade --upgrade "$D-$A"
+				TRACE sudo sbuild-apt "$D-$A" apt-get install "$PKGLIST"
+				TRACE sudo sbuild-update --clean --autoclean --autoremove "$D-$A"
 			fi
 		else
 			# Update chroot
@@ -392,8 +392,8 @@ do
 			copyInKeyrings
 			addExtraRepositories
 
-			TRACE sudo sbuild-apt $D-$A apt-get update
-			TRACE sudo sbuild-update --update --dist-upgrade --upgrade --clean --autoclean --autoremove $D-$A
+			TRACE sudo sbuild-apt "$D-$A" apt-get update
+			TRACE sudo sbuild-update --update --dist-upgrade --upgrade --clean --autoclean --autoremove "$D-$A"
 		fi
 	done
 done
